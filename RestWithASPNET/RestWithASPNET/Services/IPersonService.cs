185224c84940cd1ae1f6d3809db5257d10f35286
Gabriel Brassaroto <gabriel.brassaroto@gmail.com>
2021-11-26 15:29:43 -0300

    Service creation

---
+++ RestWithASPNET/RestWithASPNET/Services/IPersonService.cs
@@ -0,0 +1,18 @@
+﻿using RestWithASPNET.Model;
+
+namespace RestWithASPNET.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person FindById(long Id);
+
+        List<Person> FindAll();
+
+        Person Update(Person person);
+
+        void Delete(long Id);
+
+    }
+}
---
+++ RestWithASPNET/RestWithASPNET/Services/Implementations/PersonServiceImplemetation.cs
@@ -0,0 +1,65 @@
+﻿using RestWithASPNET.Model;
+
+namespace RestWithASPNET.Services.Implementations
+{
+    public class PersonServiceImplemetation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long Id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        public Person FindById(long Id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName ="Gabriel",
+                LastName = "brassaroto",
+                Address = "Ibipora",
+                Gender =  "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Namae" + i,
+                LastName = "Person lstName" + i,
+                Address = "Some Adress" + 1,
+                Gender = "Maale"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
