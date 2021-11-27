f8df552b1c7a64e0e39d61d150254f1286f39e4a
Gabriel Brassaroto <gabriel.brassaroto@gmail.com>
2021-11-27 16:34:06 -0300

    Context Creation and Bank Connection

--- RestWithASPNET/RestWithASPNET/Program.cs
+++ RestWithASPNET/RestWithASPNET/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNET.Model.Context;
+using RestWithASPNET.Services;
+using RestWithASPNET.Services.Implementations;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySqlContext>(options =>
+{
+    options.UseMySql(connection, ServerVersion.AutoDetect(connection));
+});
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();//Injeção de independencia
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ RestWithASPNET/RestWithASPNET/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,47 @@
+﻿using RestWithASPNET.Model;
+using RestWithASPNET.Model.Context;
+
+namespace RestWithASPNET.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private MySqlContext _context;
+
+        public PersonServiceImplementation(MySqlContext context)
+        {
+         _context = context;
+        }
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long Id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        public Person FindById(long Id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName ="Gabriel",
+                LastName = "brassaroto",
+                Address = "Ibipora",
+                Gender =  "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
