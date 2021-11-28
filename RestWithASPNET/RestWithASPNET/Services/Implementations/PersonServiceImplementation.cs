98f5a65ddbdf61ea926e1842d97f2181a60d09bd
Gabriel Brassaroto <gabriel.brassaroto@gmail.com>
2021-11-28 13:35:40 -0300

    Fixed code deletion

--- RestWithASPNET/RestWithASPNET/Services/Implementations/PersonServiceImplementation.cs
+++ RestWithASPNET/RestWithASPNET/Services/Implementations/PersonServiceImplementation.cs
@@ -12,36 +12,79 @@ namespace RestWithASPNET.Services.Implementations
          _context = context;
         }
 
-        public Person Create(Person person)
-        {
-            return person;
-        }
-
-        public void Delete(long Id)
-        {
-            
-        }
 
         public List<Person> FindAll()
         {
             return _context.Persons.ToList();
         }
 
-        public Person FindById(long Id)
+        public Person FindById(long id)
         {
-            return new Person
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        public Person Create(Person person)
+        {
+            try
             {
-                Id = 1,
-                FirstName ="Gabriel",
-                LastName = "brassaroto",
-                Address = "Ibipora",
-                Gender =  "Male"
-            };
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return person;
         }
 
         public Person Update(Person person)
         {
+            if (!Exists(person.Id))
+                return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
             return person;
+        }
+
+        public void Delete(long Id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(Id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+        }
+
+
+
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
