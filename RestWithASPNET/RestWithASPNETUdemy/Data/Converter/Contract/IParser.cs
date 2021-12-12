fac67f7f486c104166eee0dce6e744fb516be227
Gabriel Brassaroto <Gabriel Brassaroto@DESKTOP-BUP64P5>
2021-12-12 09:39:26 -0300

    Converter PersonVO Value Object

---
+++ RestWithASPNET/RestWithASPNETUdemy/Data/Converter/Contract/IParser.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Date.Converter.Contract
+{
+    public interface IParser<O,D> // O- origiem D- Destino
+    {
+        D Parse(O origin);
+
+        IList<D> Parse(IList<O> origin);
+    }
+}
---
+++ RestWithASPNET/RestWithASPNETUdemy/Data/Converter/Implementations/PersonConverter.cs
@@ -0,0 +1,55 @@
+﻿using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Date.Converter.Contract;
+using RestWithASPNETUdemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Data.Converter.Implementations
+{
+    public class PersonConverter : IParser<PersonVO, Person>, IParser<Person, PersonVO>
+    {
+        public Person Parse(PersonVO origin)
+        {
+            if (origin == null) return null; // se origin for null retorna null
+            return new Person //cria um person passando os valor que vieram no origin
+            {
+                Id = origin.Id,
+                FirstName = origin.FirstName,
+                LastName = origin.LastName,
+                Address = origin.Address,
+                Gender = origin.Gender
+
+            };
+        }
+
+        public PersonVO Parse(Person origin)
+        {
+            if (origin == null) return null; // se origin for null retorna null
+            return new PersonVO //cria um person passando os valor que vieram no origin
+            {
+                Id = origin.Id,
+                FirstName = origin.FirstName,
+                LastName = origin.LastName,
+                Address = origin.Address,
+                Gender = origin.Gender
+
+            };
+        }
+
+        public IList<Person> Parse(IList<PersonVO> origin)
+        {
+
+            if (origin == null) return null; // se origin for null retorna null
+            return origin.Select(item => Parse(item)).ToList();
+        }
+
+
+        public IList<PersonVO> Parse(IList<Person> origin)
+        {
+            if (origin == null) return null; // se origin for null retorna null
+            return origin.Select(item => Parse(item)).ToList();
+        }
+    }
+}
