35556877d59cae69b5ca583aa16ce32c61013efd
Gabriel Brassaroto <Gabriel Brassaroto@DESKTOP-BUP64P5>
2021-12-15 16:20:58 -0300

    Custom Serialization

--- RestWithASPNET/RestWithASPNETUdemy/Data/VO/PersonVO.cs
+++ RestWithASPNET/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -1,21 +1,24 @@
-﻿namespace RestWithASPNETUdemy.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithASPNETUdemy.Data.VO
 {
 
     public class PersonVO
     {
 
- 
+        [JsonPropertyName("code")]
         public int Id { get; set; }
 
-  
+        [JsonPropertyName("name")]
         public string FirstName { get; set; }
 
-  
+        [JsonPropertyName("Last_name")]
         public string LastName { get; set; }
 
  
         public string Address { get; set; }
- 
+
+        [JsonPropertyName("sex")]
         public string Gender { get; set; }
     }
 }
